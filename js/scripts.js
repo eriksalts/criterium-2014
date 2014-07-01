1e5501ee9a6f98b434d4b997a59ef57cea3f0b57
Kraig Hufstedler <kraigory@gmail.com>
2014-07-01 00:07:08 -0500

    worked on navbar more

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+(function ($) {$(document).ready(function() {
+
+if($('body').hasClass('navbar-is-static-top')){
+  var $filter = $('#nav');
+  var $filterSpacer = $('<div />', {
+    "class": "filter-drop-spacer",
+    "height": $filter.outerHeight()
+  });
+
+
+  if ($filter.size())
+  {
+    $(window).scroll(function ()
+    {
+      if (!$filter.hasClass('navbar-fixed-top') && $(window).scrollTop() > $filter.offset().top)
+      {
+        $filter.before($filterSpacer);
+        $filter.addClass("navbar-fixed-top");
+        $filter.removeClass("navbar-static-top");
+      }
+      else if ($filter.hasClass('navbar-fixed-top')  && $(window).scrollTop() < $filterSpacer.offset().top)
+      {
+        $filter.addClass("navbar-static-top");
+        $filter.removeClass("navbar-fixed-top");
+        $filterSpacer.remove();
+      }
+    });
+  }
+}
+
+});})(jQuery);
