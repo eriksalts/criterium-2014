256994b23a00162e0f70beb7aaa5c834de327eb0
Kraig Hufstedler <kraigory@gmail.com>
2014-07-03 00:40:46 -0500

    fixed video

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $('.player').mb_YTPlayer();
 
 if($('body').hasClass('navbar-is-static-top')){
   var $filter = $('#nav');
+  var $video = $('.video-section');
   var $filterSpacer = $('<div />', {
     "class": "filter-drop-spacer",
     "height": $filter.outerHeight()
@@ -19,12 +20,14 @@ if($('body').hasClass('navbar-is-static-top')){
         $filter.before($filterSpacer);
         $filter.addClass("navbar-fixed-top");
         $filter.removeClass("navbar-static-top");
+        $video.addClass('video-fixed');
       }
       else if ($filter.hasClass('navbar-fixed-top')  && $(window).scrollTop() < $filterSpacer.offset().top)
       {
         $filter.addClass("navbar-static-top");
         $filter.removeClass("navbar-fixed-top");
         $filterSpacer.remove();
+        $video.removeClass('video-fixed');
       }
     });
   }
